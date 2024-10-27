b594a07c77a38aa6192878d2e33191abfd646868
nassim <laskrouifn@gmail.com>
2024-10-27 11:04:37 +0100

    black jack project

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-class animal():
+import random
 
-    def bark(self):
 
-        print("hawhaw")
-bobi = animal()
-bobi.bark()
+cards = []
+suits = [ "spades" , "clubs", "hearts" , "diamonds"]
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+random.shuffle(cards)
+
+card = cards.pop()
+
+
+print(card)
+
+
+
+
